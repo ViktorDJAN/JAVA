fd873ea8a99de4deea9f6feadf85775ec3df92e2
Viktor St.one <pistoletviktor@gmail.com>
2023-03-18 15:09:54 +0300

    second saving

--- src/main/java/org/example/cw/cw1/task_1.java
+++ src/main/java/org/example/cw/cw1/task_1.java
@@ -1,17 +1,23 @@
 package org.example.cw.cw1;
 
 import java.util.Scanner;
+
 public class task_1 {
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
         System.out.print("Input your number: ");
         int num = a.nextInt();
-        int a1 = num/100;
-        int a2 = num%100/10;
-        int a3 = num%10;
-        int act1 = a1*a2*a3;
-        int act2 = a1+a2+a3;
-        int res = act1-act2;
-        System.out.println(res);
+        System.out.println(funk(num));
+    }
+    public static int funk(int n) {
+        int sum = 0;
+        int multy = 1;
+        while(n!=0){
+             sum += n%10;
+             multy *= n%10;
+             n = n/10;
+        }
+        int res = multy - sum;
+        return res;
     }
 }
